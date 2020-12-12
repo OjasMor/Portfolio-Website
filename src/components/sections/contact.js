2fd879ebdd2745e3d124fcecb7398987024792b7
OjasMor <ojas.m233@gmail.com>
2020-12-11 20:38:22 -0800

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,8 +51,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I'm currently looking for sofware based internships, and would love to
+        chat about my past experiences. I'm usually very fast at responding to emails, so
+        I'll get back to you in around a day. 
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
