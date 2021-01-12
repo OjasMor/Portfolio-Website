9a56407d323d6f0d514b84ee4fa875779d1adfee
OjasMor <ojas.m233@gmail.com>
2021-01-12 12:11:28 -0800

    Update skills list

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'C/C++', 'Gatsby', 'OpenCV', 'Python'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
